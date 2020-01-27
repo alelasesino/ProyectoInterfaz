a6dc2f50905533f7ed33f1d47ea821f29cc96244
ALEJANDRO <ALEJANDRO@DESKTOP-8QQCSOH>
2020-01-27 19:56:57 +0100

    Form1 Added

---
+++ ProyectoInterfaz/FormMain.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoInterfaz {
+    public partial class FormMain:System.Windows.Forms.Form {
+        public FormMain() {
+            InitializeComponent();
+        }
+
+        private void clienteBindingNavigatorSaveItem_Click(object sender,EventArgs e) {
+            this.Validate();
+            this.clienteBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.segurosDataSet);
+
+        }
+
+        private void Form1_Load(object sender,EventArgs e) {
+            // TODO: esta línea de código carga datos en la tabla 'segurosDataSet.cliente' Puede moverla o quitarla según sea necesario.
+            this.clienteTableAdapter.Fill(this.segurosDataSet.cliente);
+
+        }
+    }
+}
