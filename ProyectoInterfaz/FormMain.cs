05642396495c1f5cc62877b999f537a48d7defb4
Alejandro <alelasesino99@hotmail.es>
2020-01-28 12:25:05 +0100

    Cagada

--- ProyectoInterfaz/FormMain.cs
+++ ProyectoInterfaz/FormMain.cs
@@ -9,9 +9,18 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace ProyectoInterfaz {
-    public partial class FormMain:System.Windows.Forms.Form {
+    public partial class FormMain: Form {
+
+        private DateTimePicker cellDateTimePicker = new DateTimePicker();
+
         public FormMain() {
             InitializeComponent();
+
+            polizaDataGridView.Controls.Add(cellDateTimePicker);
+            cellDateTimePicker.Visible = false;
+            cellDateTimePicker.Format = DateTimePickerFormat.Custom;
+            cellDateTimePicker.TextChanged += new EventHandler(cellDateTimePickerChanged);
+
         }
 
         private void clienteBindingNavigatorSaveItem_Click(object sender,EventArgs e) {
@@ -21,12 +30,19 @@ namespace ProyectoInterfaz {
 
         }
 
+        private void polizaBindingNavigatorSaveItem_Click(object sender,EventArgs e) {
+            this.Validate();
+            this.polizaBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.segurosDataSet);
+
+        }
+
         private void Form1_Load(object sender,EventArgs e) {
             // TODO: esta línea de código carga datos en la tabla 'segurosDataSet.poliza' Puede moverla o quitarla según sea necesario.
             this.polizaTableAdapter.Fill(this.segurosDataSet.poliza);
+            //this.polizaDataGridView.Columns[0].Visible = false;
             // TODO: esta línea de código carga datos en la tabla 'segurosDataSet.cliente' Puede moverla o quitarla según sea necesario.
             this.clienteTableAdapter.Fill(this.segurosDataSet.cliente);
-
         }
 
         private void bindingNavigatorAddNewItem_Click(object sender,EventArgs e) {
@@ -35,5 +51,40 @@ namespace ProyectoInterfaz {
             //form.Show();
 
         }
+
+        private void polizaDataGridView_CellBeginEdit(object sender,DataGridViewCellCancelEventArgs e) {
+
+            switch(polizaDataGridView.Columns[e.ColumnIndex].Name){
+            case "fecha":
+                var _Rectangle = polizaDataGridView.GetCellDisplayRectangle(e.ColumnIndex, e.RowIndex, true);
+                cellDateTimePicker.Size = new Size(_Rectangle.Width, _Rectangle.Height);
+                cellDateTimePicker.Location = new Point(_Rectangle.X, _Rectangle.Y);
+                cellDateTimePicker.Visible = true;
+            break;
+            }    
+
+        }
+
+        private void cellDateTimePickerChanged(object sender, EventArgs e) {
+        
+            polizaDataGridView.CurrentCell.Value = cellDateTimePicker.Text.ToString();
+        
+        }
+
+        private void polizaDataGridView_CancelRowEdit(object sender,QuestionEventArgs e) {
+            cellDateTimePicker.Visible = false;
+        }
+
+        private void polizaDataGridView_CellEndEdit(object sender,DataGridViewCellEventArgs e) {
+            
+            if(polizaDataGridView.Columns[e.ColumnIndex].Name == "fecha") {
+
+                polizaDataGridView.CurrentCell.Value = cellDateTimePicker.Text.ToString();
+                cellDateTimePicker.Visible = false;
+
+            }
+            
+        }
     }
+
 }
